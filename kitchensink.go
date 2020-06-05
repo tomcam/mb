35a5ef93337675bd723f58c00b6fb074607ce400
Tom Campbell <tomcampbell@gmail.com>
2020-06-05 07:44:55 -0700

    Add correct viewbox to svg file in kitchensink

--- kitchensink.go
+++ kitchensink.go
@@ -20,6 +20,7 @@ type embedded struct {
 /* SVG file of an exciting 100x100px gray box */
 var svgFile = `<?xml version="1.0" encoding="utf-8"?>
 <svg  xmlns="http://www.w3.org/2000/svg">
+  viewBox="0 0 100 100" 
   <rect x="0" y="0" width="100" height="100" style="fill: rgb(216, 216, 216);"/>
 </svg>
 `
