2e206b050c2ee5d13f2dbbe441dbd61754905a48
Tom Campbell <tomcampbell@gmail.com>
2020-06-06 18:15:42 -0700

    Fix embedded svg file

--- kitchensink.go
+++ kitchensink.go
@@ -19,8 +19,11 @@ type embedded struct {
 
 /* SVG file of an exciting 100x100px gray box */
 var svgFile = `<?xml version="1.0" encoding="utf-8"?>
-<svg  xmlns="http://www.w3.org/2000/svg">
-  viewBox="0 0 100 100" 
+
+<svg version="1.1"
+     baseProfile="full"
+     width="100" height="100"
+     xmlns="http://www.w3.org/2000/svg">
   <rect x="0" y="0" width="100" height="100" style="fill: rgb(216, 216, 216);"/>
 </svg>
 `
