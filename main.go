5f35d2eb2336fe552a59c2ff59db53f943f424e0
Tom Campbell <tomcampbell@gmail.com>
2020-06-27 12:32:12 -0700

    Put issues in the right place

--- main.go
+++ main.go
@@ -1,9 +1,5 @@
 package main
 
-import (
-	"fmt"
-)
-
 func main() {
 	// Get the execution environment ready.
 	App := newDefaultApp()
@@ -11,7 +7,4 @@ func main() {
 	if err := App.Cmd.Execute(); err != nil {
 		App.QuitError(err)
 	}
-	fmt.Println("VERSION 4:30pm")
-	fmt.Println("* Document the v validator. Write an article about it")
-	fmt.Println("* Put something in the headtags directory")
 }
