706f9693c98a451fc1259a256e789acbb9081af7
Tom Campbell <tomcampbell@gmail.com>
2020-12-07 18:25:20 -0800

    Correct for case of no sidebar

--- .mb/scriptclose/fillins.js
+++ .mb/scriptclose/fillins.js
@@ -4,9 +4,7 @@ function bodyScroll(){if(scrollTimer != -1){clearTimeout(scrollTimer);}scrollTim
 function sidebarHeight() {
 s=document.getElementById('sidebar');
 a=document.getElementById('article'); 
-h=a.offsetHeight+'px';
-s.style.height=h;
-//s.style.height='100vh';
+if (s != null) {h=a.offsetHeight+'px';s.style.height=h;}
 }
 document.onreadystatechange = function () {
 if (document.readyState == "interactive") {
