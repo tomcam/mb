10e196dc3772f117b74ebe70b651054e8fe0b164
Tom Campbell <tomcampbell@gmail.com>
2020-12-07 23:58:04 -0800

    Handle case where sidebar is longer than article

--- .mb/scriptclose/fillins.js
+++ .mb/scriptclose/fillins.js
@@ -4,7 +4,21 @@ function bodyScroll(){if(scrollTimer != -1){clearTimeout(scrollTimer);}scrollTim
 function sidebarHeight() {
 s=document.getElementById('sidebar');
 a=document.getElementById('article'); 
-if (s != null) {h=a.offsetHeight+'px';s.style.height=h;}
+if (s != null && a != null) {
+  // If there's a sidebar,
+  ha=a.offsetHeight;
+  hs=s.offsetHeight;
+  // If the article is longer, make
+  // the sidebar as long as the article.
+  if (ha>hs) {
+    h=a.offsetHeight+'px';s.style.height=h;
+  }
+  // If the sidebar is longer than the
+  // article, make the article as long
+  // as the sidebar.
+  else{
+    h=s.offsetHeight+'px';a.style.height=h;
+  }
 }
 document.onreadystatechange = function () {
 if (document.readyState == "interactive") {
