b3a15fe8b75e025baf2ab7100a76a44b3a1493fd
Tom Campbell <tomcampbell@gmail.com>
2020-12-10 08:42:52 -0800

    Refactor & redocument common script features

--- .mb/scriptclose/fillins.js
+++ .mb/scriptclose/fillins.js
@@ -1,32 +1,40 @@
 <script>
 var scrollTimer = -1;
-function bodyScroll(){if(scrollTimer != -1){clearTimeout(scrollTimer);}scrollTimer = window.setTimeout(sidebarHeight, 100);}window.onresize=sidebarHeight;
+function bodyScroll()
+{
+  if(scrollTimer != -1){
+    clearTimeout(scrollTimer);
+  }
+  scrollTimer = window.setTimeout(sidebarHeight, 100);
+}
 function sidebarHeight() {
-s=document.getElementById('sidebar');
-a=document.getElementById('article'); 
-if (s != null && a != null) {
-  // If there's a sidebar,
-  ha=a.offsetHeight;
-  hs=s.offsetHeight;
-  // If the article is longer, make
-  // the sidebar as long as the article.
-  if (ha>hs) {
-    h=a.offsetHeight+'px';s.style.height=h;
+  s=document.getElementById('sidebar');
+  a=document.getElementById('article'); 
+  if (s != null && a != null) {
+    // If there's a sidebar,
+    ha=a.offsetHeight;
+    hs=s.offsetHeight;
+    // If the article is longer, make
+    // the sidebar as long as the article.
+    if (ha>hs) {
+      h=a.offsetHeight+'px';s.style.height=h;
+    }
+    // If the sidebar is longer than the
+    // article, make the article as long
+    // as the sidebar.
+    else{
+      h=s.offsetHeight+'px';a.style.height=h;
+    }
   }
-  // If the sidebar is longer than the
-  // article, make the article as long
-  // as the sidebar.
-  else{
-    h=s.offsetHeight+'px';a.style.height=h;
-  }
-}
-document.onreadystatechange = function () {
-if (document.readyState == "interactive") {
-  // Init or start code here
-  bodyScroll();
-}
 } 
-  
-  </script>
-<body onscroll="bodyScroll();">
+document.onreadystatechange = function () {
+  if (document.readyState == "interactive") {
+    // Init or start code here
+    bodyScroll();
+  }
+} 
+window.onresize=sidebarHeight;
+/* ES5 works fine */
+document.getElementsByTagName('body')[0].onscroll=bodyScroll();
+</script>
 
